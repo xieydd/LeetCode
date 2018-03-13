05398e5e554b1e50cc3ece457f618700de239fdb
xieydd <xieydd@gmail.com>
2018-03-13 22:13:58 +0800

    1-twosum

---
+++ 1-twosum.java
@@ -0,0 +1,17 @@
+/**
+ * description:LeetCode:1_twosum
+ * author:xieydd
+ * date:Tue Mar 13 20:16:50 CST 2018
+ */
+class TwoSum{
+    public int[] void twoSum_1(int[] nums,int target) {
+        for(int i = 0,i<nums.length(),i++) {
+            for(j+i+1,j<nums.length(),j++) {
+                if(nums[j] = target-nums[i]) {
+                    return new int[] {i,j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution")
+    }
+}
